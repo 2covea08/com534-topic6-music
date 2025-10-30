d9f12481a8a8b370539dcddccff40e6706a0eed5
Ash Coventry <2covea08@solent.ac.uk>
2025-10-30 16:53:25 +0000

    Created Music interface and edited album and single classes to add getAllSongs and getPlayingTime functions

--- src/Album.kt
+++ src/Album.kt
@@ -1,3 +1,18 @@
-class Album(val songs: List<Song>) {
+class Album(val songs: List<Song>): Music {
+
+    fun getSongsByArtists(artist: String) : List<Song> {
+     //  return songs.filter { song -> song.artist == artist }
+        return songs.filter { it.artist == artist }
+    }
+
+    override fun getAllSongs(): List<Song> {
+        return songs
+    }
+
+    override fun getPlayingTime(): Double {
+        var runningTime = 0.0
+        songs.forEach { runningTime += it.playingTime }
+        return runningTime
+    }
 
 }
